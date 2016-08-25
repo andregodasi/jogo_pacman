885e43f2a2515782211d504076821f5ebe61deb8
André Gonçalves da Silva <andre.godasi@gmail.com>
2016-08-25 16:17:53 -0300

    jogo pacman finalizado

--- mapa.h
+++ mapa.h
@@ -1,16 +1,44 @@
-typedef struct mapa {
-    char** matriz;
-    int linhas;
-    int colunas;
-} MAPA;
+#ifndef _MAPA_H_
+#define _MAPA_H_
 
-typedef struct posicao {
-    int x;
-    int y;
-} POSICAO;
 
-void alocamapa();
-void lemapa();
-void liberamapa();
-void imprimemapa();
-void encontramapa(MAPA* m, POSICAO* p, char c);
+#define HEROI '@'
+#define VAZIO '.'
+#define PAREDE_VERTICAL '|'
+#define PAREDE_HORIZONTAL '-'
+#define FANTASMA 'F'
+#define PILULA 'P'
+
+struct mapa {
+	char** matriz;
+	int linhas;
+	int colunas;
+};
+
+typedef struct mapa MAPA;
+
+void alocamapa(MAPA* m);
+void lemapa(MAPA* m);
+void liberamapa(MAPA* m);
+
+struct posicao {
+	int x;
+	int y;
+};
+
+typedef struct posicao POSICAO;
+
+int encontramapa(MAPA* m, POSICAO* p, char c);
+
+int ehvalida(MAPA* m, int x, int y);
+int ehparede(MAPA* m, int x, int y);
+int ehpersonagem(MAPA* m, char personagem, int x, int y);
+
+void andanomapa(MAPA* m, int xorigem, int yorigem,
+	int xdestino, int ydestino);
+
+void copiamapa(MAPA* destino, MAPA* origem);
+
+int podeandar(MAPA* m, char personagem, int x, int y);
+
+#endif
